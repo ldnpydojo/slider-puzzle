374411a0dae15212baf16ba1d402f24e8702925c
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-02-07 20:57:41 +0000

    Rename board to slider

--- team1/slider.py
+++ team1/slider.py
@@ -1,31 +1,31 @@
 from collections import defaultdict
 import random
 
-EMPTY = -1
+EMPTY = ' '
 
 
-def random_board(n, randomize=random.shuffle):
+def random_slider(n, randomize=random.shuffle):
     l = range(n ** 2 - 1)
     randomize(l)
     l.append(EMPTY)
-    b = defaultdict(dict)
+    slider = defaultdict(dict)
     for i in range(n):
         for j in range(n):
-            b[i][j] = l[i * n + j]
-    return b
+            slider[i][j] = l[i * n + j]
+    return slider
 
 
-def ordered_board(n):
-    return random_board(n, randomize=lambda x: x)
+def ordered_slider(n):
+    return random_slider(n, randomize=lambda x: x)
 
 
-def print_board(b):
-    for i in range(len(b)):
-        print ' '.join(str(c) for c in b[i].values())
+def print_slider(slider):
+    for i in range(len(slider)):
+        print ' '.join(str(c) for c in slider[i].values())
 
 
-def check_board(b):
-    n = len(b)
+def check_slider(slider):
+    n = len(slider)
     for i in range(n):
         for j in range(n):
-            assert b[i][j] == (EMPTY if i == n - 1 and j == n - 1 else i * n + j)
+            assert slider[i][j] == (EMPTY if i == n - 1 and j == n - 1 else i * n + j)
--- team1/test_slider.py
+++ team1/test_slider.py
@@ -1,9 +1,9 @@
 from slider import *
 
 
-def test_check_ordered_board():
-    check_board(ordered_board(3))
+def test_check_ordered_slider():
+    check_slider(ordered_slider(3))
 
 if __name__ == '__main__':
-    print_board(ordered_board(3))
-    print_board(random_board(3))
+    print_slider(ordered_slider(3))
+    print_slider(random_slider(3))
