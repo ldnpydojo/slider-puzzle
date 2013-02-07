2abe2e90fffb9e8055b0979e8d23890db1377d79
markeganfuller <mark.egan-fuller@mvfglobal.com>
2013-02-07 20:44:22 +0000

    added check if finished and refactored last number

--- team2/sliding.py
+++ team2/sliding.py
@@ -4,24 +4,28 @@ class Board:
 
     def __init__(self, n):
         self.n = n
-        self.board = dict(((i,j),j*n+i+1) for i in range(n) for j in range(n))
-        self.board[(n-1,n-1)] = ' '
+        self.board = dict(((i,j),j*n+i) for i in range(n) for j in range(n))
+        self.last = self.n**2 - 1
 
     def __repr__(self):
         res = ""
         for j in range(self.n):
             for i in range(self.n):
-                res += "%s"%self.board[(i,j)]
+                number = self.board[(i,j)]
+                if number != self.last:
+                    res += "%s" % (number + 1)
+                else:
+                    res += ' '
             res += "\n"
         return res
 
 
     def possible_moves(self):
-        res = []
+        res = [] 
         gap = None
-        for i in  range(self.n):
-            for j in range(self.n):
-                    if self.board[(i,j)] == ' ':
+        for j in  range(self.n):
+            for i in range(self.n):
+                    if self.board[(i,j)] == self.last:
                         gap = (i,j)
                         break
             if gap is not None:
@@ -41,14 +45,26 @@ class Board:
 
         return res
 
+    def check_if_finished(self):
+        number_list = []
+        for j in range(self.n):
+            for i in range(self.n):
+                number = self.board[(i, j)]
+                if number != j*self.n + i and (number != self.last and i == self.n -1 and j == self.n -1):
+                    return False
+        return True
+
 
     def swap(self, a, b):
         self.board[a], self.board[b] = self.board[b], self.board[a]
 
+
 if __name__ == "__main__":
     b = Board(3)
     print b
+    print b.check_if_finished()
     b.swap((2, 1), (2, 2))
     print b
     print b.possible_moves()
+    print b.check_if_finished()
 
