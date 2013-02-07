d8dcb239ee8012cfa89edb4880587663fefc9aef
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-02-07 20:48:19 +0000

    Refactor

--- team1/test_slider.py
+++ team1/test_slider.py
@@ -1,32 +1,4 @@
-from collections import defaultdict
-import random
-
-
-def random_board(n, randomize=random.shuffle):
-    l = range(n**2 - 1)
-    randomize(l)
-    l.append(-1)
-    b = defaultdict(dict)
-    for i in range(n):
-        for j in range(n):
-            b[i][j] = l[i*n+j]
-    return b
-
-
-def ordered_board(n):
-    return random_board(n, randomize=lambda x: x)
-
-
-def print_board(b):
-    for i in range(len(b)):
-        print ' '.join(str(c) for c in b[i].values())
-
-
-def check_board(b):
-    n = len(b)
-    for i in range(n):
-        for j in range(n):
-            assert b[i][j] == (-1 if i == n-1 and j == n-1 else i*n+j)
+from slider import *
 
 
 def test_check_ordered_board():
